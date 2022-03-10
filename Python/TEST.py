57b30856dd5ca9703339152e71ac81a33a5505d5
Phillip Radasevic <98527875+Phxiil@users.noreply.github.com>
2022-03-10 17:57:18 +0000

    l

--- Python/TEST.py
+++ Python/TEST.py
@@ -1,5 +1,5 @@
 import re
 
-with open("../CyberSecurity2022.pcap","rb")as file:
-    rePattern = re.compile(rb'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\.[0-9a-fA-F]{4}\.[0-9a-fA-F]{4})')
+with open("CyberSecurity2022.pcap","rb")as file:
+    rePattern = re.compile(rb'(?:[[:xdigit:]]{2}([-:]))(?:[[:xdigit:]]{2}\1){4}[[:xdigit:]]{2}$')
     print(rePattern.findall(file.read()))
