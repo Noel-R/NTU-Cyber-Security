aaa675801880b576b761d5b93089d49b81cf87bd
Noel-R <73253924+Noel-R@users.noreply.github.com>
2022-03-13 18:18:45 +0000

    New version, cleaner fully commented.
    Need to add task 3 and 4 to it however.

--- Python/test.py
+++ Python/test.py
@@ -2,5 +2,9 @@ import re
 
 with open("CyberSecurity2022.pcap", 'rb') as f:
 
+    data = f.read()[24:].decode('utf-8', 'ignore')
+
     rePattern = re.compile(r'(?:http:\/\/)[a-z, A-Z 0-9 .]+(?:.top)')
-    print(rePattern.findall(f.read()[24:].decode('utf-8', 'ignore')))
+    reWebPatter = re.compile(r'(?:http|https)?(?:\:\/\/)(?:www\.)?(?:[a-zA-z,.!?-]\w+(?:\.)?)+(?:\.com|\.net|\.top){1}(?:\/(?:[a-zA-z0-9,.!?-]\w+)*)*')
+    print(rePattern.findall(data))
+    print(reWebPatter.findall(data))
