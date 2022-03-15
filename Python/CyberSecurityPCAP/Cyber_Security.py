81b4be656ce0582820a8b7b6121c47db1e83e0fe
Noel <N0931129@my.ntu.ac.uk>
2022-03-15 15:54:18 +0000

    Re-haul, complete? probably.

--- Python/CyberSecurityPCAP/Cyber_Security.py
+++ Python/CyberSecurityPCAP/Cyber_Security.py
@@ -3,6 +3,7 @@
 import sys, datetime, re
 import more_itertools as mit
 
+
 # This is a class made for the ease of use and file handling.
 # The class is responsible for all operations with the file.
 # Mainly: Decomposing the file, documenting it.
@@ -18,13 +19,15 @@ class PacketCapture:
         self.packetList = []
         self.packetAmount = 0
         self.MagicNumber = None
+        self.filename = filename
 
         # Compiled Regular Expressions to search for.
 
-        self.websitePattern = re.compile(rb'(?:http|https)?(?:\:\/\/)(?:www\.)?(?:[a-zA-z,.!?-]\w+(?:\.)?)+(?:\.com|\.net|\.top){1}(?:\/(?:[a-zA-z0-9,.!?-]\w+)*)*')
+        self.websitePattern = re.compile(
+            rb'(?:http|https)?(?:\:\/\/)(?:www\.)?(?:[a-zA-z,.!?-]\w+(?:\.)?)+(?:\.com|\.net|\.top){1}(?:\/(?:[a-zA-z0-9,.!?-]\w+)*)*')
 
         # Try to open the file, return and delete this object if bad filepath.
-        # If successfull, start the main decomposition of the file with initialRun().
+        # If successful, start the main decomposition of the file with initialRun().
 
         try:
 
@@ -336,7 +339,7 @@ class PacketCapture:
                             Raw Data: {currPacket[1]}
                             Regex Search: {self.websitePattern.findall(currPacket[1])}
 
-                  -= FIRST | BACK | SAVE | RETURN | NEXT | LAST =-
+                  -= FIRST | PREV | SAVE | RETURN | NEXT | LAST =-
         """)
 
         # Returning to the prior function.
@@ -344,19 +347,35 @@ class PacketCapture:
         return
 
     def dhcpPacketDisect(self):
+
+        # Disecting a DHCP Packet, works universally, however tailored to the first packet (no packet select).
+
+        # Remove the other headers from the packet.
+
         packet = bytes(self.packetList[0][1][42:])
+
+        # Define the regex pattern to search for the client, usually ends in '-PC'.
+
         rePCsearch = re.compile(b'\w+-PC{1}')
-        print(f"packet type: {type(packet)}")
+
+        # Define a dictionary with specific byte positions.
+
         dhcpPacketDict = dict(op=packet[0], htype=packet[1],
                               hlen=packet[2],
                               hops=packet[3], xid=f"0x{packet[4:8].hex()}",
                               secs=int.from_bytes(packet[8:10], self.MagicNumber), flags=packet[10:12],
                               ciaddr=packet[12:16].hex('.'), yiaddr=packet[16:20].hex('.'),
                               siaddr=packet[20:24].hex('.'), giaddr=packet[24:28].hex('.'),
-                              mac=packet[28:43].hex().strip("0"), sname=packet[43:107].hex(), file=packet[107:234].hex(),
+                              mac=packet[28:43].hex().strip("0"), sname=packet[43:107].hex(),
+                              file=packet[107:234].hex(),
                               options=packet[234:], hostPcName=rePCsearch.findall(packet[234:]))
 
+        # Format the Addresses to human readable format, i.e ipv4 and Mac address.
+
         for key, val in dhcpPacketDict.items():
+
+            # If "ADDR" or "mac" in the key, in dhcpPacketDict, format the cool.
+
             if "addr" in key:
                 val = val.split(".")
                 print(val)
@@ -366,73 +385,202 @@ class PacketCapture:
                 val = ["".join(c) for c in mit.grouper(2, val)]
                 dhcpPacketDict[key] = ':'.join(val)
 
+        # Print the information, present options.
+
         print(
-        f"""
+            f"""
                         -= DHCP Packet Information =-
-                        
+
                                 Packet Number 1
-                            
+
                               -= Contents =-
-                              
-                        Message Type: {dhcpPacketDict.get("op")}
-                        Hardware Address Type: {dhcpPacketDict.get("htype")}
-                        Hardware Address Length: {dhcpPacketDict.get("hlen")}
-                        Hops: {dhcpPacketDict.get("hops")}
-                        Transaction ID: {dhcpPacketDict.get("xid")}
-                        Seconds Elapsed: {dhcpPacketDict.get("secs")}
-                        Flags: {dhcpPacketDict.get("flags")}
-                        Client Internet Address: {dhcpPacketDict.get("ciaddr")}
-                        Your Internet Address: {dhcpPacketDict.get("yiaddr")}
-                        Server Address: {dhcpPacketDict.get("siaddr")}
-                        Gateway Address: {dhcpPacketDict.get("giaddr")}
-                        Client Hardware Address: {dhcpPacketDict.get("mac")}
-                        Server Name: {dhcpPacketDict.get("sname")}
-                        Boot File: {dhcpPacketDict.get("file")}
-                        Options: {dhcpPacketDict.get("options")}
-                        PC Host Name: {dhcpPacketDict.get("hostPcName")}
+
+                Message Type: {dhcpPacketDict.get("op")}
+                Hardware Address Type: {dhcpPacketDict.get("htype")}
+                Hardware Address Length: {dhcpPacketDict.get("hlen")}
+                Hops: {dhcpPacketDict.get("hops")}
+                Transaction ID: {dhcpPacketDict.get("xid")}
+                Seconds Elapsed: {dhcpPacketDict.get("secs")}
+                Flags: {dhcpPacketDict.get("flags")}
+                Client Internet Address: {dhcpPacketDict.get("ciaddr")}
+                Your Internet Address: {dhcpPacketDict.get("yiaddr")}
+                Server Address: {dhcpPacketDict.get("siaddr")}
+                Gateway Address: {dhcpPacketDict.get("giaddr")}
+                Client Hardware Address: {dhcpPacketDict.get("mac")}
+                Server Name: {dhcpPacketDict.get("sname")}
+                Boot File: {dhcpPacketDict.get("file")}
+                Options: {dhcpPacketDict.get("options")}
+                PC Host Name: {dhcpPacketDict.get("hostPcName")}
+                        
+                                -= RETURN =- 
+
         """)
 
-        inMenu = True
-        while inMenu:
+        # while the user is looking the data, wait for their input.
+        while True:
+
+            # Except errors.
+
             try:
+
+                # Get User input.
+
                 choice = str(input("Choice: ")).lower()
 
                 if choice in ["return", "ret", "r"]:
+
                     return
+
                 else:
+
                     raise ValueError
+
             except ValueError:
+
                 print("Choice is not valid, please use 'RETURN' to return to menu.")
                 continue
 
     def task3(self):
-        print("Task 4")
+
+        # Open file again, search through it with the regular expression and record output.
+
+        with open(self.filename, 'rb') as file:
+            websites = self.websitePattern.findall(file.read()[0:])
+
+        matches = []
+
+        # Check every website in the websites array for '.top', if so append it to another array.
+
+        for website in websites:
+            if b'.top' in website:
+                matches.append(website)
+
+        print(
+            f"""
+                    -= Task 3 =-
+            The user tried access the following
+            websites with the '.top' domain name
+            ending. 
+
+                    -= Results =-
+
+            websites: {matches}
+
+
+                    -= RETURN =-
+        """)
+
+        # while the user is looking the data, wait for their input.
+        while True:
+
+            # Except errors.
+
+            try:
+
+                # Get User input.
+
+                choice = str(input("Choice: ")).lower()
+
+                if choice in ["return", "ret", "r"]:
+
+                    return
+
+                else:
+
+                    raise ValueError
+
+            except ValueError:
+
+                print("Choice is not valid, please use 'RETURN' to return to menu.")
+                continue
 
     def task4(self):
-        pass
 
+        # Open file and search through it with regular expression, record all matches.
+
+        with open(self.filename, 'rb') as file:
+            websites = self.websitePattern.findall(file.read())
+
+        # Search for the first '.top' entry, find out the position of it then use it to find the search engine and reccomended site.
+
+        i = 0
+        done = False
+        for website in websites:
+            if b'.top' in website and not done:
+                MaliciousSite = websites[i]
+                done2 = False
+                e = 2
+                while not done2:
+                    if b'bing' in websites[i - e] or b'Baidu' in websites[i - e] or b'yahoo' in websites[i - e]:
+                        SearchEngine = websites[i - e]
+                        ReccomendedSite = websites[i - e + 1]
+                        done2 = True
+                    else:
+                        e += 1
+                done = True
+            i += 1
+
+        print(
+            f"""
+                        -= Task 4 =-
+            Find the search engine the user tried    
+            to access before they had been attacked
+            along with what site was reccomended.
+
+                      -= Information =-
+
+          Search Engine Address: {SearchEngine}
+          Site Reccomended: {ReccomendedSite}
+          Malicious Redirect: {MaliciousSite}
+
+                        -= RETURN =- 
+        """)
+
+        # while the user is looking the data, wait for their input.
+        while True:
+
+            # Except errors.
+
+            try:
+
+                # Get User input.
+
+                choice = str(input("Choice: ")).lower()
+
+                if choice in ["return", "ret", "r"]:
+
+                    return
+
+                else:
+
+                    raise ValueError
+
+            except ValueError:
+
+                print("Choice is not valid, please use 'RETURN' to return to menu.")
+                continue
 
 
 def menu():
-
     # Print the main menu text, basic information and options menu.
 
     print(
         f"""
                 -=Packet Capture Analyser=-
-                
+
             This is a basic tool made for the course
             Cyber Security, NTU. It is used for the
             basic analysis of .pcap files without
             the use of modules such as dpkt and scapy.
-            
+
                         -=Options=-
-                    
+
                 1 [ Check Global Header ]
                 2 [   Search Packets    ]
                 3 [    DHCP Packet      ]
-                4 [       Task 4        ]
-                5 [        Quit         ]
+                4 [       Task 3        ]
+                5 [       Task 4        ]
+                6 [        Quit         ]
 
     """)
 
@@ -446,7 +594,7 @@ def menu():
 
             option = int(input("\nChoice: "))
 
-            if option in range(1, 6):
+            if option in range(1, 7):
 
                 # Return the desired option to the prior function.
 
@@ -459,6 +607,7 @@ def menu():
         except ValueError:
 
             print("\nPlease choose a valid Option. [1 ,2, 3, 4, 5]\n")
+
 
 # If the name of this file is main in the package, this is automatically ran.
 
@@ -480,7 +629,6 @@ if __name__ == '__main__':
             if len(sys.argv) > 1:
 
                 if ".pcap" in sys.argv[1]:
-
                     # If there is a valid filename/path in the command line arguments,
                     # use this to create a PacketCapture Object and set first run to false.
 
@@ -535,11 +683,13 @@ if __name__ == '__main__':
                         pcapClass.globalHeader()
                     elif option == 2:  # Packet Search
                         pcapClass.searchPackets()
-                    elif option == 3:  # Task 3
+                    elif option == 3:  # Task 2
                         pcapClass.dhcpPacketDisect()
-                    elif option == 4:  # Task 4
+                    elif option == 4:  # Task 3
+                        pcapClass.task3()
+                    elif option == 5:  # Task 4
                         pcapClass.task4()
-                    elif option == 5:  # Quit
+                    elif option == 6:  # Quit
 
                         # If quitting, close the file and quit the program.
 
